db6f723ac9677760ad38fb0d4f65892f4881703f
felipe-pmm <felipe.vidal.mesquita@gmail.com>
2023-03-10 19:24:34 -0300

    UI

--- components/layout/Dashboard.tsx
+++ components/layout/Dashboard.tsx
@@ -15,10 +15,10 @@ export default function DashboardLayout({ children }) {
     <div className="bg-light-900 dark:bg-dark-900 flex min-h-screen flex-col">
       <div id="notifications" />
       <Navbar />
-      <div className="w-screen overflow-auto flex flex-1 bg-light-900 text-light-50 dark:bg-dark-900 dark:text-dark-50">
+      <main className="flex flex-1 overflow-auto bg-light-900 text-light-50 dark:bg-dark-900 dark:text-dark-50">
         <Sidebar />
         <div className="flex flex-1 p-4 w-full">{children}</div>
-      </div>
+      </main>
       <Footer />
       {notificationMessage && 
         <QueryNotification messageText={notificationMessage} messageType={notificationType} />
